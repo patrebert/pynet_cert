b82cce3ddb20d9e4abc89d74579ddeb3513bdf55
Pat Rebert <patrebert@yahoo.com>
2016-09-29 12:41:16 -0700

    update exercises

--- class9/different_directory/mytest/world.py
+++ class9/different_directory/mytest/world.py
@@ -5,7 +5,6 @@ def func3():
 class MyClass:
 
     def __init__(self,arg1, arg2, arg3):
-        print "MyClass __init__"
         self.arg1 = arg1
         self.arg2 = arg2
         self.arg3 = arg3
@@ -19,11 +18,6 @@ class MyClass:
         print "  %s %s %s" %(self.arg1, self.arg2, self.arg3)
 
 class MyChildClass(MyClass):
-
-    def __init__(self,var1,var2,var3):
-        print "MyChildClass __init__"
-        MyClass.__init__(self, var1, var2, var3)
-
     def  hello(self):
         print "This is the overridden 'hello' method of MyChildClass speaking"
         print "  %s %s %s" %(self.arg3, self.arg2, self.arg1)
@@ -38,7 +32,6 @@ def main():
     hell = MyClass('1','2','3')
     hell.hello()
     hell.not_hello()
-    print "CREATE MyChildClass object"
     hell2 = MyChildClass('1','2','3')
     hell2.hello()
 
---
+++ class9/ex8/mytest/__init__.py
@@ -0,0 +1,6 @@
+from mytest.simple import func1
+from mytest.whatever import func2
+from mytest.world import func3
+from mytest.world import testclass
+from mytest.world import MyClass
+__all__ = ['func1', 'func2', 'func3', 'testclass', 'MyClass']
